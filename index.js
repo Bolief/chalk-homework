f7a91ba524dc46f3517ffce2650c78e09493b951
Francis Bolie <boliefrancis7@gmail.com>
2026-03-01 00:05:00 -0600

    add thank you message to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import chalk from 'chalk';
 
 console.log(chalk.blue("Hello Professor!"));
-console.log(chalk.green.bold("This is my Chalk homework."));
+console.log(chalk.red.bold("This is my Chalk homework."));
 console.log(chalk.yellow("Git + NPM + Chalk working correctly!"));
+console.log(chalk.green.italic("Thank you for the lesson!"));
