9cd480a4decf3f6ce4d38dc739ee06cb777ebd02
Francis Bolie <boliefrancis7@gmail.com>
2026-02-28 23:46:45 -0600

    Initial commit with chalk setup

---
+++ index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue("Hello Professor!"));
+console.log(chalk.green.bold("This is my Chalk homework."));
+console.log(chalk.yellow("Git + NPM + Chalk working correctly!"));
