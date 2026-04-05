f8682616f5b907323af079ff8f21c8eb903ea8a0
Francis Bolie <boliefrancis7@gmail.com>
2026-04-05 05:45:08 -0500

    remove code

--- index.js
+++ index.js
@@ -4,4 +4,3 @@ console.log(chalk.blue("Hello Professor!"));
 console.log(chalk.red.bold("This is my Chalk homework."));
 console.log(chalk.yellow("Git + NPM + Chalk working correctly!"));
 console.log(chalk.green.italic("Thank you for the lesson!"));
-confirm("Do you like the colors?") ? console.log(chalk.cyan("Great!")) : console.log(chalk.magenta("Oh no!"));
